59f54a6ef37c70f795f549ae2062334d21c15e65
takashi <mr.takamuu@gmail.com>
2021-06-17 11:02:42 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+let youtuber = {
+  list: {
+    business: {
+      youtuber: [
+        { name: "しまぶー", age: 20, teachProgramming() {} },
+        { name: "あっちゃん", age: 37, teachHistory() {} }
+      ],
+      teach() {}
+    },
+    entertainment: {
+      youtuber: [{ name: "ヒカキン" }, { name: "はじめしゃちょー" }],
+      makeSmile() {}
+    }
+  },
+  plan() {},
+  uploadVideo() {}
+};
