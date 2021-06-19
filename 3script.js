2a84d48883cc42eb5c545de2747a4b4785741ea7
takashi <mr.takamuu@gmail.com>
2021-06-20 07:44:51 +0900

    DOM操作完了

--- 3script.js
+++ 3script.js
@@ -2,6 +2,13 @@ const messageField = document.getElementById('message-field')
 const button = document.getElementById('message-button')
 const messageList = document.getElementById('message-list')
 
-button.onclick = () => {
+const addMessage = () => {
   messageList.insertAdjacentHTML('afterbegin', `<p>${messageField.value}</p>`)
+  messageField.value = ''
+}
+
+button.onclick = () => addMessage()
+
+messageField.onkeydown = (e) => {
+  if (e.keyCode == '13') addMessage()
 }
