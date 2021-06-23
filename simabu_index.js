4f913310c66710c7a45a696341387e1648545520
takashi <mr.takamuu@gmail.com>
2021-06-23 15:42:37 +0900

    simabu_completed

---
+++ simabu_index.js
@@ -0,0 +1,5 @@
+import { name, name2, log } from "./user.js";
+
+log("foo");
+
+document.body.textContent = name + name2;
---
+++ simabu_user.js
@@ -0,0 +1,10 @@
+export const name = "ピカチュウ";
+export const name2 = "ライチュウ";
+
+function log(value) {
+  console.log(value);
+}
+
+export { log };
+
+// export { name };
