142720542d4faae328ad585ff5cb69dc8d8f6c2e
takashi <mr.takamuu@gmail.com>
2021-07-10 12:28:16 +0900

    #13 Prototype Object

--- jsprimer/index.js
+++ jsprimer/index.js
@@ -531,25 +531,42 @@
 // console.log(obj.nest);
 // console.log(cloneObj.nest === obj.nest); // => true
 
-// 引数の`obj`を浅く複製したオブジェクトを返す
-const shallowClone = (obj) => {
-    return Object.assign({}, obj);
-};
-// 引数の`obj`を深く複製したオブジェクトを返す
-function deepClone(obj) {
-    const newObj = shallowClone(obj);
-    // プロパティがオブジェクト型であるなら、再帰的に複製する
-    Object.keys(newObj)
-        .filter(k => typeof newObj[k] === "object")
-        .forEach(k => newObj[k] = deepClone(newObj[k]));
-    return newObj;
-}
-const obj = {
-    level: 1,
-    nest: {
-        level: 2
-    }
-};
-const cloneObj = deepClone(obj);
-// `nest`オブジェクトも再帰的に複製されている
-console.log(cloneObj.nest === obj.nest); // => false
+// // 引数の`obj`を浅く複製したオブジェクトを返す
+// const shallowClone = (obj) => {
+//     return Object.assign({}, obj);
+// };
+// // 引数の`obj`を深く複製したオブジェクトを返す
+// function deepClone(obj) {
+//     const newObj = shallowClone(obj);
+//     // プロパティがオブジェクト型であるなら、再帰的に複製する
+//     Object.keys(newObj)
+//         .filter(k => typeof newObj[k] === "object")
+//         .forEach(k => newObj[k] = deepClone(newObj[k]));
+//     return newObj;
+// }
+// const obj = {
+//     level: 1,
+//     nest: {
+//         level: 2
+//     }
+// };
+// const cloneObj = deepClone(obj);
+// // `nest`オブジェクトも再帰的に複製されている
+// console.log(cloneObj.nest === obj.nest); // => false
+
+// // プロトタイプオブジェクト
+// const obj = {
+//     "key": "value"
+// };
+// console.log(obj.toString === Object.prototype.toString); 
+// console.log(Object.prototype.toString()); 
+// console.log(obj.toString());
+
+// オブジェクトのインスタンスにtoStringメソッドを定義
+// const customObject = {
+//     toString() {
+//         return "custom value";
+//     }
+// };
+// console.log(customObject.toString()); // => "custom value"
+
