57eee37e55c0247563b78902b02227e59a0f64ac
takashi <mr.takamuu@gmail.com>
2021-07-21 15:52:17 +0900

    JSPrimer #22 非同期処理Promise.rejectまで

--- jsprimer/index.js
+++ jsprimer/index.js
@@ -1447,14 +1447,20 @@
 //     }, 1000 * Math.random());
 // }
 
-function delay(timeoutMs) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve();
-        }, timeoutMs);
-    });
-}
-// `then`メソッドで成功時のコールバック関数だけを登録
-delay(10).then(() => {
-    console.log("10ミリ秒後に呼ばれる");
+// function delay(timeoutMs) {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             resolve();
+//         }, timeoutMs);
+//     });
+// }
+// // `then`メソッドで成功時のコールバック関数だけを登録
+// delay(10).then(() => {
+//     console.log("10ミリ秒後に呼ばれる");
+// });
+
+// Promise.resolve
+// const fulfilledPromise = Promise.resolve(); と同じ意味
+const fulfilledPromise = new Promise((resolve) => {
+    resolve();
 });
